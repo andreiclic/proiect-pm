984884d10dab6d784e0118a89f6ee17fb5387690
Andrei Clicinschi <andrei.clic.501@gmail.com>
2021-05-23 20:13:51 +0300

    buttons

--- lightmeter/lightmeter.ino
+++ lightmeter/lightmeter.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+// digital pins definitions
+#define SET_ISO_BUTTON 2
+#define SET_ND_BUTTON 3
+#define ACTIVATE_ND_SWITCH 4
 
+// button states
+#define BUTTON_PRESSED 0
+#define BUTTON_NOT_PRESSED 1
+
+// button states
+int SET_ISO_BUTTON_state;
+int SET_ND_BUTTON_state;
+
+void setup() {
+  // SET ISO BUTTON init
+  pinMode(SET_ISO_BUTTON, INPUT_PULLUP);
+  SET_ISO_BUTTON_state = BUTTON_NOT_PRESSED;
+
+  // SET ND BUTTON init
+  pinMode(SET_ND_BUTTON, INPUT_PULLUP);
+  SET_ND_BUTTON_state = BUTTON_NOT_PRESSED;
+
+  // ACTIVATE ND SWITCH init
+
+  // serial init for debugging
+  Serial.begin(9600);
+  Serial.println("Lightmeter is ready!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read the button states
+  int SET_ISO_BUTTON_state_tmp = digitalRead(SET_ISO_BUTTON);
+  int SET_ND_BUTTON_state_tmp = digitalRead(SET_ND_BUTTON);
 
+  // SET ISO BUTTON update
+  if ( SET_ISO_BUTTON_state_tmp != SET_ISO_BUTTON_state ) {
+    if ( SET_ISO_BUTTON_state_tmp == BUTTON_PRESSED ) {
+      Serial.println("Set ISO button was pressed");
+    }
+    
+    SET_ISO_BUTTON_state = SET_ISO_BUTTON_state_tmp;
+  }
+
+  // SET ND BUTTON update
+  if ( SET_ND_BUTTON_state_tmp != SET_ND_BUTTON_state ) {
+    if ( SET_ND_BUTTON_state_tmp == BUTTON_PRESSED ) {
+      Serial.println("Set ND button was pressed");
+    }
+    
+    SET_ND_BUTTON_state = SET_ND_BUTTON_state_tmp;
+  }
 }
